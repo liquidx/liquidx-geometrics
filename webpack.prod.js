85ca0b92a6a67debdbabd94318410dbfa55cddd9
Alastair Tse <alastair@liquidx.net>
2020-01-04 11:48:05 +0900

    remove unneeded babel minifier

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const MinifyPlugin = require('babel-minify-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
